92bb14d1dd74ac433e8b6b0197a0b1f96228f99a
nithinreddyu8-cell <nithinreddyu8@gmail.com>
2026-08-05 21:40:25 +0530

    19

--- 19.cpp
+++ 19.cpp
@@ -3,15 +3,15 @@
 int getValueFromUser() // this function now returns an integer value
 {
  	std::cout << "Enter an integer: ";
-	int input{};
+	int input;
 	std::cin >> input;
 
 	return input; // return the value the user entered back to the caller
 }
-
+ss
 int main()
 {
-	int num { getValueFromUser() }; // initialize num with the return value of getValueFromUser()
+	int num = getValueFromUser() ; // initialize num with the return value of getValueFromUser()
 
 	std::cout << num << " doubled is: " << num * 2 << '\n';
 

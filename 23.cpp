942d1217082897427c575ece23be896f4330b3d2
nithinreddyu8-cell <nithinreddyu8@gmail.com>
2026-08-06 21:56:52 +0530

    23

---
+++ 23.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int getValue()
+{
+    std::cout << "Enter an integer: ";
+
+    int x{};
+    std::cin >> x;
+    return x;
+}
+
+void printCalculation(int x, int y, int z)
+{
+    std::cout << x + (y * z);
+}
+
+int main()
+{
+    int a{ getValue() }; // will execute first
+    int b{ getValue() }; // will execute second
+    int c{ getValue() }; // will execute third
+
+    printCalculation(a, b, c); // this line is now unambiguous
+
+    return 0;
+}

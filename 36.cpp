93437fa6a45123c8aa43b2516ff33be05b3ac4ce
nithinreddyu8-cell <nithinreddyu8@gmail.com>
2026-08-13 17:48:33 +0530

    37

---
+++ 36.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void print(int i,int sum){
+
+    if(i<1){
+        cout<<sum<<" ";
+        return;
+    }
+    print(i-1,sum+i);
+    }
+    
+int main(){
+    int n;
+    cin>>n;
+    print(n,0);
+}

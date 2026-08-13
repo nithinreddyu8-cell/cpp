38cc4361adb8bf2e089e2ace4da62a69119fbdbd
nithinreddyu8-cell <nithinreddyu8@gmail.com>
2026-08-13 15:36:58 +0530

    35

---
+++ 35.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void print(int i,int n){
+    if(i<1)return;
+    else
+    cout<<i<<" ";
+    print(i-1,n);
+}
+int main(){
+    int n;
+    cin>>n;
+    print(n,n);
+}

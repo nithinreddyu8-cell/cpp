c00e3301a31afd8ccb1d6084c7ddea4f873fc51d
nithinreddyu8-cell <nithinreddyu8@gmail.com>
2026-08-31 19:03:30 +0530

    MERGE SORT

---
+++ 71.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(int arr[], int low, int mid, int high)
+{
+    vector<int> temp;
+
+    int i = low;
+    int j = mid + 1;
+
+    while (i <= mid && j <= high)
+    {
+        if (arr[i] <= arr[j])
+        {
+            temp.push_back(arr[i]);
+            i++;
+        }
+        else
+        {
+            temp.push_back(arr[j]);
+            j++;
+        }
+    }
+
+    while (i <= mid)
+    {
+        temp.push_back(arr[i]);
+        i++;
+    }
+
+    while (j <= high)
+    {
+        temp.push_back(arr[j]);
+        j++;
+    }
+
+    for (int k = low; k <= high; k++)
+    {
+        arr[k] = temp[k - low];
+    }
+}
+
+void mergeSort(int arr[], int low, int high)
+{
+    if (low >= high)
+        return;
+
+    int mid = (low + high) / 2;
+
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid + 1, high);
+
+    merge(arr, low, mid, high);
+}
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    mergeSort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}

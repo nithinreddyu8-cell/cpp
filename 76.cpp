8afcd0e4f75571cdc5a6edfc6ee1694d3dd1d4a2
nithinreddyu8-cell <nithinreddyu8@gmail.com>
2026-09-06 20:46:07 +0530

    POSTORDER TRAVERSAL

---
+++ 76.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+void postorder(Node* root) {
+    if (root == nullptr)
+        return;
+
+    // Left
+    postorder(root->left);
+
+    // Right
+    postorder(root->right);
+
+    // Root
+    cout << root->data << " ";
+}
+
+int main() {
+    Node* root = new Node(1);
+
+    root->left = new Node(2);
+    root->right = new Node(3);
+
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    cout << "Postorder Traversal: ";
+    postorder(root);
+
+    return 0;
+}

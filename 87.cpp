81d030b0dabeb2c806b31d4d409e11d38c203ebb
nithinreddyu8-cell <nithinreddyu8@gmail.com>
2026-09-20 21:50:22 +0530

    TWO SUM PROBLEM (brute)

---
+++ 87.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> arr = {2, 6, 5, 8, 11};
+    int target = 14;
+
+    for (int i = 0; i < arr.size(); i++) {
+        for (int j = i + 1; j < arr.size(); j++) {
+
+            if (arr[i] + arr[j] == target) {
+                cout << arr[i] << " " << arr[j];
+                return 0;
+            }
+        }
+    }
+
+    cout << "No pair found";
+
+    return 0;
+}
